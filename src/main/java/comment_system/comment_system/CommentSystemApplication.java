2b545c13523e2047c8e729a3471daf76f81dfdf0
Zein <zeinkasar123@gmail.com>
2023-01-02 16:58:36 +0100

    web_socket
    webSocket configratie gemaakt

--- src/main/java/comment_system/comment_system/CommentSystemApplication.java
+++ src/main/java/comment_system/comment_system/CommentSystemApplication.java
@@ -1,9 +1,14 @@
 package comment_system.comment_system;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class CommentSystemApplication {
 
 	public static void main(String[] args) {

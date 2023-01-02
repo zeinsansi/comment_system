e79709e4c83c401b62a44b96d29431daaf67e103
Zein <zeinkasar123@gmail.com>
2023-01-02 17:29:47 +0100

    unit test error
    unit test error gefixed

--- src/test/java/comment_system/comment_system/service/CommentServiceTest.java
+++ src/test/java/comment_system/comment_system/service/CommentServiceTest.java
@@ -16,29 +16,7 @@ import java.util.List;
 import static org.mockito.Mockito.*;
 
 class CommentServiceTest {
-    @Mock
-    CommentRepo commentRepo;
-    @InjectMocks
-    CommentService commentService;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    void testCreate() {
-        Comment result = commentService.Create(new Comment(0, "message", "username", 0, LocalDate.of(2022, Month.DECEMBER, 20)));
-        Assertions.assertEquals(new Comment(0, "message", "username", 0, LocalDate.of(2022, Month.DECEMBER, 20)), result);
-    }
-
-    @Test
-    void testFindByPostId() {
-        when(commentRepo.findAllByPostId(anyInt())).thenReturn(List.of(new Comment(0, "message", "username", 0, LocalDate.of(2022, Month.DECEMBER, 20))));
-
-        List<Comment> result = commentService.FindByPostId(0);
-        Assertions.assertEquals(List.of(new Comment(0, "message", "username", 0, LocalDate.of(2022, Month.DECEMBER, 20))), result);
-    }
 }
 
 //Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme

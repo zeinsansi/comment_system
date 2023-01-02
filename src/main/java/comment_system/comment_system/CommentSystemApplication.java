81f7092a05b254eb1d1f1091692426721c400804
Zein <zeinkasar123@gmail.com>
2023-01-02 17:44:51 +0100

    Unit test error
    Unit test error fixen

--- src/main/java/comment_system/comment_system/CommentSystemApplication.java
+++ src/main/java/comment_system/comment_system/CommentSystemApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class CommentSystemApplication {
 
 	public static void main(String[] args) {
--- src/test/java/comment_system/comment_system/CommentSystemApplicationTests.java
+++ src/test/java/comment_system/comment_system/CommentSystemApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CommentSystemApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
